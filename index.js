e0567abe575d5625ab01b71d609101d61617c6a6
“saschakala” <mxsaschakala@gmail.com>
2020-12-04 23:14:40 -0500

    commit tree ffcb8756286ab74c50faaf0dc93fd8385819e926

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+class Polygon {
+    constructor(arr) {
+        this.sides = arr
+    }
+
+    get countSides() {
+        return this.sides.length
+    }
+
+    get perimeter() {   
+      return this.sides.reduce(function(total, current){
+          return total + current
+      })
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.sides[0] + this.sides[1] < this.sides[2]) {
+            return false;
+        } else if (this.sides[1] + this.sides[2] < this.sides[0]) {
+            return false;
+        } else if (this.sides[0] + this.sides[2] < this.sides[1]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if (this.sides.every(side => side === this.sides[0])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    get area() {
+        return this.sides[0] ** 2
+    }
+}
+
+
+
